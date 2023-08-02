5597f0d77fcfa04dcbd02eafae825cf74dd6e021
Boitumelo <bcrachoshi@gmail.com>
2023-08-02 10:27:28 +0200

    printf code

---
+++ revision/ALX/0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string literal using printf
+ * Return : 0 upon success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
