cef7267c7ef600de63899433028a6ea0d9fe7157
Boitumelo <bcrachoshi@gmail.com>
2023-08-03 14:14:51 +0200

    sum of two function

---
+++ revision/ALX/0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return : sum of intergers
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum=a+b;
+
+	return (sum);
+}
