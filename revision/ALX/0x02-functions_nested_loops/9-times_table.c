fd81a44478636599d4f2f6470a69d36146bbe0b6
Boitumelo <bcrachoshi@gmail.com>
2023-08-03 18:26:55 +0200

    times table understood

--- revision/ALX/0x02-functions_nested_loops/9-times_table.c
+++ revision/ALX/0x02-functions_nested_loops/9-times_table.c
@@ -6,5 +6,35 @@
 
 void times_table(void)
 {
+	int a;
+	int b;
+	int c;
 
+	for ( a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <=9; b++)
+		{
+			c = a*b;
+			if (b==0)
+                       {
+                            putchar(c + '0');
+                            putchar(',');
+                        }
+                        else if (c <10)
+                        {
+                            putchar(' ');
+                            putchar(' ');
+                            putchar(c+'0');
+                            putchar(',');
+                        }
+                        else
+                        {
+                                putchar(' ');
+                                putchar((c / 10) + '0');
+                                putchar((c % 10) + '0');
+                                putchar(',');
+                        }
+                }
+                putchar('\n');
+        }
 }
