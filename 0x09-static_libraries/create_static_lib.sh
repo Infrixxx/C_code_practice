a828c4c057b6d85e3be3a50216e2b9b599d66a52
Boitumelo <bcrachoshi@gmail.com>
2024-01-03 08:36:50 +0200

    static library file update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 #script to compile without linking '-c'
 #create object files
-gcc -c *.c -pedantic -Wall -Wextra -std=gnu89
+gcc -c *.c -pedantic -Werror -Wextra -Wall -std=gnu89
 #create library
-ar rc create_static_lib.sh *.c
-
+ar rc liball.a *.c
+#create and update index
+ranlib liball.a
 #remove object files after library creation
 rm -f *.o
