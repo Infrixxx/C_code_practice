fca2989274cecde5076ec8a9ab5c6f4f00697ca3
Boitumelo <bcrachoshi@gmail.com>
2024-01-21 06:04:24 +0200

    fix to grid file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,69 +3,73 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **h; /* pointer to everything */
-    int j;
-    int i;
-
-    h = (int **)malloc(height * sizeof(int *));
+    int **grid;
+    int row;
+    int column;
     
-    /* error handling if row is empty*/
-    if (h == NULL)
+    grid=(int **)malloc(height*sizeof(int*));
+    /*incase there is no  height input*/
+    if (grid==NULL)
     {
-        return NULL; /* failed to allocate memory for rows */
-    }
-
-    for (j = 0; j < height; j++)
-    {
-        h[j] = (int *)malloc(width * sizeof(int));
-        if (h[j] == NULL)
-        {
-         
-	   /* failed to allocate memory for columns in this row */
-            for (i = 0; i < j; i++)
-            {
-                free(h[i]);
-            }
-	    /* if if there's no column entry, rows will be empty*/
-            free(h);
             return NULL;
-        }
-
-        for (i = 0; i < width; i++)
-        {
-            h[j][i] = 0;
-        }
     }
 
-    return h; /* return the pointer to the matrix */
+    /*if there is height input*/
+    for(row=0;row<height;row++)
+    {
+        grid[row]=(int *)malloc(width*sizeof(int));
+        
+        /*incase width is empty*/
+        if (grid[row]==NULL)
+        {
+            /* Free previously allocated rows */
+            for (int i = 0; i < row; i++)
+            {
+                free(grid[i]);
+            }
+            free(grid);
+            return (NULL);
+        }
+        
+        /* Initialize values in the row to 0 */
+        for (column=0;column<width;column++)
+        {
+            grid[row][column]=0;
+    
+        }
+    }
+    
+    
+    return (grid);   
 }
 
-void print_grid(int **grid, int width, int height)
+void print_grid(int **grid,int width,int height)
 {
-    int i, j;
-    for (i = 0; i < height; i++)
+    int a;
+    int b;
+    
+    for (a = 0; a < height; a++)
     {
-        for (j = 0; j < width; j++)
+        for (b = 0; b < width; b++)
         {
-            printf("%d ", grid[i][j]);
+            printf("%d ",grid[a][b]);
         }
         printf("\n");
     }
 }
-
-int main()
+int main() 
 {
+    // Write C code here
     int **a;
-
-    a = alloc_grid(2, 2);
     
-    a[0][1]=7;
+    a= alloc_grid(3,5);
+    /*altering contents of array within bounds*/
+    a[1][2]=77;
+    a[2][2]=66;
+    a[0][2]=55;
     
-    if (a != NULL)
-    {
-        print_grid(a, 2, 2);
-    }
+    /*print grid, desired amount*/
+    print_grid(a,2,2);
 
     return 0;
 }
-
