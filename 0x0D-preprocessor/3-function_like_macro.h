7a340c555ec83f5f4ba6f2ac6437ba26b4072723
Boitumelo <bcrachoshi@gmail.com>
2024-01-26 06:03:34 +0200

    macros def, function and file guards

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(X) ((X) < 0 ? -X:X)   /*if x is negative we get positive, if positive, we get positive*/
+
+#endif /*end if for Function like macro def*/
